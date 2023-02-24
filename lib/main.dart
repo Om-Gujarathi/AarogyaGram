e1080a85b0c373c8b89ae2d60d2abe63059bf3ee
Om Gujarathi <veryhappyom@gmail.com>
2023-02-24 16:05:17 +0530

    Added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: prefer_const_constructors, unused_import
 
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:vjti/Screens/AuthenticationScreens/LogInScreen.dart';
@@ -51,6 +50,9 @@ class MyApp extends StatelessWidget {
             }
             return const LogInPage();
           }),
+      // home: Scaffold(
+      //   body: Center(child: Text('HI')),
+      // ),
     );
   }
 }
